ea982a97adb25f52b1d4a687522dbac774c34733
TigerSkin <1012399286@qq.com>
2025-07-05 04:25:28 +0800

    橘福福框架（10%）

---
+++ zsim/sim_progress/Character/Jufufu/HuweiManagerClass.py
@@ -0,0 +1,20 @@
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from sim_progress.Character.character import Character
+
+
+class HuweiManager:
+    """虎威管理器，这里有虎威所有的数据机制以及业务逻辑"""
+    def __init__(self, char_instance: "Character"):
+        self.char = char_instance
+
+    def check_myself(self):
+        """虎威自检，内置CD啥的，若条件满足则run_myself"""
+        pass
+
+    def run_myself(self, signal: int):
+        """根据传入的更新信号做出决策——到底是打平A还是打连携技或是其他"""
+        # 注：通过信号来控制虎威逻辑只是一个初步设计，可能会更改。
+        pass
+
+    # 尚不确定虎威是否需要静默、被打断等逻辑，一边写一边看吧。
